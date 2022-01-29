7f1bc6d82a481a44173519354a7d3860b3c293ba
arek544 <33511737+arek544@users.noreply.github.com>
2022-01-29 08:57:37 +0000

    time logging for reading dataset

--- src/datasets.py
+++ src/datasets.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pandas as pd
-
+import time 
 from pathlib import Path
 
 class Dataset:
     
     def __init__(self, path):
+        timer_start = time.time()
         self.df = self.load_data(path)
         self.X = self.df[["x", "y"]].values
         self.y = self.df["real_cluster"].values
         self.name = Path(path).stem
         self.n_dimentions = self.X.shape[1]
         self.n_rows = self.X.shape[0]
+        self.runtime = timer_start - time.time()
     
     def load_data(self, path):
         header = ["x", "y", "real_cluster"]
